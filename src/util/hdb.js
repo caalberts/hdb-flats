1d7428f0efb675648d2bf338e77bfcdb8a3558dd
Yong Jun <yjthong.2009@smu.edu.sg>
2015-12-03 17:37:08 +0800

    Updated hdb.js

--- src/util/hdb.js
+++ src/util/hdb.js
@@ -2,24 +2,28 @@ import _ from 'lodash'
 import fetch from 'node-fetch'
 
 export default function getData (town, flatType) {
-  const dataURL = 'https://data.gov.sg/api/action/datastore_search?resource_id=8d2112ca-726e-4394-9b50-3cdf5404e790&q={"town": "' + town + '", "flat_type":"' + flatType + '"}'
+  const dataURL = 'https://data.gov.sg/api/action/datastore_search?resource_id=8d2112ca-726e-4394-9b50-3cdf5404e790&limit=&q={"town": "' + town + '", "flat_type":"' + flatType + '"}'
 
-  fetch(dataURL).then(data => data.json())
+  return fetch(dataURL).then(data => data.json())
     .then(json => {
-    // console.log(json)
-    var records = json.result.records
-    var grouped = _.groupBy(records, record => record.month)
-    var result = _.reduce(grouped, (result, recs, key) => {
-      var avg_price = recs.reduce((avg, rec) => avg + +rec.resale_price, 0)
-      avg_price = avg_price / (recs.length || 1)
-      avg_price = Math.round(avg_price)
-      result.push({
-        'month': key,
-        'txn_count': recs.length,
-        'avg_price': avg_price
-      })
-      return result
-    }, [])
-    // console.log(result)
-  })
+      var records = json.result.records
+      var grouped = _.groupBy(records, record => record.month)
+      var result = _.reduce(grouped, (aggregate, recs, key) => {
+        var avg_price = recs.reduce((avg, rec) => avg + +rec.resale_price, 0)
+        avg_price = avg_price / (recs.length || 1)
+        avg_price = Math.round(avg_price)
+        aggregate.push({
+          'month': key,
+          'txn_count': recs.length,
+          'avg_price': avg_price
+        })
+        return aggregate
+      }, [])
+      console.log(result)
+      return {
+        'town': town,
+        'type': flatType,
+        'data': result
+      }
+    })
 }
