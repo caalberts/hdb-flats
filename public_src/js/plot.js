f19ac4d7e4b376188c9e12407508c6ea04e0ad9d
Albert <albertlimca@gmail.com>
2015-12-11 11:31:11 +0800

    Include transactions list in plot page

--- public_src/js/plot.js
+++ public_src/js/plot.js
@@ -65,7 +65,7 @@ function plotChart () {
         title: chart + ' of HDB Resale Price in ' + town,
         autosize: false,
         width: 1000,
-        height: 600,
+        height: 500,
         margin: {
           l: 50,
           r: 50,
@@ -78,6 +78,10 @@ function plotChart () {
         }
       }
       Plotly.newPlot(plotSpace, data, layout)
+
+      plotSpace.on('plotly_click', click => {
+        listAllTransactions(town, click.points[0].data.name, click.points[0].x)
+      })
     })
 }
 
@@ -87,6 +91,31 @@ function createDropDown (list, selector) {
     option.textContent = item
     document.getElementById(selector).appendChild(option)
   })
+}
+
+function listAllTransactions (town, type, date) {
+  const resID = [
+    'a3f3ad06-5c05-4177-929f-bb9fffccebdd',
+    'e119f1a2-e528-4535-adaf-2872b60dbf0a',
+    '8d2112ca-726e-4394-9b50-3cdf5404e790'
+  ]
+  const resource = (Date.parse(date) < new Date('2005-01-01')) ? resID[0] : (Date.parse(date) < new Date('2012-03-01')) ? resID[1] : resID[2]
+  console.log(resource)
+  const dataURL = 'https://data.gov.sg/api/action/datastore_search?resource_id=' + resource + '&q={"town": "' + town + '", "flat_type":"' + type + '", "month":"' + date.slice(0, 7) + '"}'
+  console.log(dataURL)
+  window.fetch(dataURL).then(data => data.json())
+    .then(json => {
+      if (document.getElementById('transactions-ul')) document.getElementById('ul').remove()
+      const ul = document.createElement('ul')
+      ul.setAttribute('id', 'transactions-ul')
+      json.result.records.forEach(transaction => {
+        console.log(transaction)
+        const li = document.createElement('li')
+        li.textContent = transaction.town.trim() + ' ' + transaction.flat_type.trim() + ' ' + transaction.month + ' $' + transaction.resale_price
+        ul.appendChild(li)
+      })
+      document.getElementById('transactions-list').appendChild(ul)
+    })
 }
 
 window.onload = function () {
