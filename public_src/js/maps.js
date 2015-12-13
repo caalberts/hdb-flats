2f9cd9711640e0ca871e815139838561c96c902b
Albert <albertlimca@gmail.com>
2015-12-13 23:54:48 +0800

    Use classes for maps

--- public_src/js/maps.js
+++ public_src/js/maps.js
@@ -4,70 +4,87 @@ import range from 'lodash.range'
 import padLeft from 'lodash.padleft'
 import createDropDown from './createDropDown'
 
-function drawForm () {
-  const types = [
-    '1 Room', '2 Room', '3 Room', '4 Room',
-    '5 Room', 'Executive', 'Multi-Generation'
-  ]
-  const monthsList = range(2001, 2016).map(year => {
-    return range(1, 13).map(month => {
-      return year.toString() + '-' + padLeft(month.toString(), 2, '0')
-    })
-  })
-  const months = monthsList.reduce((a, b) => a.concat(b)).reverse()
+class App {
+  constructor () {
+    this.typeSelection = document.getElementById('select-type')
+    this.monthSelection = document.getElementById('select-month')
+    this.mapDiv = document.getElementById('map')
+  }
 
-  createDropDown(types, 'select-type', '3 Room')
-  createDropDown(months, 'select-month', '2015-09')
-
-  document.getElementById('select-type').addEventListener('change', () => getData())
-  document.getElementById('select-month').addEventListener('change', () => getData())
-}
-
-function getData () {
-  /* Data points defined as an array of LatLng objects */
-  let mapData = []
-  const typeSelection = document.getElementById('select-type')
-  const type = typeSelection.options[typeSelection.selectedIndex].text
-
-  const monthSelection = document.getElementById('select-month')
-  const month = monthSelection.options[monthSelection.selectedIndex].text
-
-  const url = window.location.protocol + '//' + window.location.host + '/heatmap?type=' + type + '&month=' + month
-
-  console.log('fetch', url)
-  window.fetch(url).then(res => res.json())
-    .then(results => {
-      results.forEach(result => {
-        result.dataPoints.forEach(transaction => {
-          if (!(transaction.lat === 1.352083 && transaction.lng === 103.819836)) {
-            const tick = {
-              location: new google.maps.LatLng(transaction.lat, transaction.lng),
-              weight: transaction.weight
-            }
-            mapData.push(tick)
-          }
-        })
+  drawForm () {
+    const types = [
+      '1 Room', '2 Room', '3 Room', '4 Room',
+      '5 Room', 'Executive', 'Multi-Generation'
+    ]
+    const monthsList = range(2001, 2016).map(year => {
+      return range(1, 13).map(month => {
+        return year.toString() + '-' + padLeft(month.toString(), 2, '0')
       })
-      drawHeatmap(mapData)
     })
+    const months = monthsList.reduce((a, b) => a.concat(b)).reverse()
+
+    createDropDown(types, 'select-type', '3 Room')
+    createDropDown(months, 'select-month', '2015-09')
+
+    this.typeSelection.addEventListener('change', () => this.drawHeatmap())
+    this.monthSelection.addEventListener('change', () => this.drawHeatmap())
+  }
+
+  drawHeatmap () {
+    const heatmap = new Heatmap(
+      this.typeSelection.options[this.typeSelection.selectedIndex].text,
+      this.monthSelection.options[this.monthSelection.selectedIndex].text,
+      this.mapDiv
+    )
+    heatmap.getData()
+  }
 }
 
-function drawHeatmap (locations) {
-  if (locations.length === 0) console.log('no data')
-  const singapore = new google.maps.LatLng(1.352083, 103.819836)
+class Heatmap {
+  constructor (type, month, plotId) {
+    this.type = type
+    this.month = month
+    this.mapData = []
+  }
 
-  const map = new google.maps.Map(document.getElementById('map'), {
-    center: singapore,
-    zoom: 11
-  })
-  const heatmap = new google.maps.visualization.HeatmapLayer({
-    data: locations,
-    radius: 15
-  })
-  heatmap.setMap(map)
+  getData () {
+    const url = window.location.protocol + '//' + window.location.host + '/heatmap?type=' + this.type + '&month=' + this.month
+
+    window.fetch(url).then(res => res.json())
+      .then(results => {
+        results.forEach(result => {
+          result.dataPoints.forEach(transaction => {
+            if (!(transaction.lat === 1.352083 && transaction.lng === 103.819836)) {
+              const tick = {
+                location: new google.maps.LatLng(transaction.lat, transaction.lng),
+                weight: transaction.weight
+              }
+              this.mapData.push(tick)
+            }
+          })
+        })
+        this.plotHeatmap(this.mapData)
+      })
+  }
+
+  plotHeatmap (locations) {
+    if (locations.length === 0) console.log('no data')
+    const singapore = new google.maps.LatLng(1.352083, 103.819836)
+
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: singapore,
+      zoom: 11
+    })
+    const googleHeatmap = new google.maps.visualization.HeatmapLayer({
+      data: locations,
+      radius: 15
+    })
+    googleHeatmap.setMap(map)
+  }
 }
 
 window.onload = function () {
-  drawForm()
-  getData()
+  const app = new App()
+  app.drawForm()
+  app.drawHeatmap()
 }
