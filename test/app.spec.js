ec3f6c682e344bf9eb311487fcfbcb873e613ebd
Albert <albertlimca@gmail.com>
2015-12-15 16:58:34 +0800

    Add test

---
+++ test/app.spec.js
@@ -0,0 +1,45 @@
+/* global describe it */
+import app from '../src/app.js'
+import chai from 'chai'
+import request from 'supertest'
+
+const expect = chai.expect
+const NOT_FOUND = 404
+const OK = 200
+
+describe('/GET /towns', function () {
+  this.timeout(10000)
+  it('should respond with JSON data', done => {
+    request(app).get('/towns?flat_type=3 Room&town=Tampines')
+      .expect('Content-Type', /json/)
+      .expect(OK)
+      .end(done)
+  })
+  it('should provide a list of time series as an array', done => {
+    request(app).get('/towns?town=Tampines')
+      .expect(res => {
+        expect(res.body).to.be.an('array')
+      })
+      .end(done)
+  })
+  it('should respond NOT_FOUND if query is invalid', done => {
+    request(app).get('/towns?town=Kuala Lumpur')
+      .expect(NOT_FOUND)
+      .end(done)
+  })
+})
+
+describe('/GET /heatmap', function () {
+  this.timeout(10000)
+  it('should respond with JSON data', done => {
+    request(app).get('/heatmap?flat_type=3 Room&month=2015-08')
+      .expect('Content-Type', /json/)
+      .expect(OK)
+      .end(done)
+  })
+  it('should respond NOT_FOUND if query is invalid', done => {
+    request(app).get('/heatmap?flat_type=closet')
+      .expect(NOT_FOUND)
+      .end(done)
+  })
+})
