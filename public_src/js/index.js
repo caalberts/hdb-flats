5e1a03f39cca75281f688b6a2110bfed643942f7
Albert <albertlimca@gmail.com>
2015-12-28 23:09:58 +0800

    Add single page navigation

--- public_src/js/index.js
+++ public_src/js/index.js
@@ -1,16 +1,33 @@
 import 'whatwg-fetch'
 import { TimeSeries, Maps } from './app.js'
 
+Array.from(document.querySelectorAll('.nav-item')).forEach(nav => {
+  nav.addEventListener('click', event => {
+    event.preventDefault()
+    Array.from(document.querySelectorAll('.nav-item')).forEach(nav => nav.classList.remove('active'))
+    event.target.parentElement.classList.add('active')
+    window.history.pushState(null, '', event.target.textContent.toLowerCase())
+    Array.from(document.getElementById('chart-container').children)
+      .forEach(child => child.remove())
+    Array.from(document.getElementById('chart-detail').children)
+      .forEach(child => child.remove())
+    Array.from(document.querySelector('.navbar-right').children)
+      .forEach(child => child.remove())
+    route().drawChart()
+  })
+})
+
 window.onload = function () {
-  let app
-  const active = document.querySelector('.active')
-  switch (active.textContent) {
-    case 'Charts':
-      app = new TimeSeries()
-      break
-    case 'Maps':
-      app = new Maps()
-      break
+  route().drawChart()
+}
+
+function route () {
+  switch (window.location.pathname) {
+    case '/charts':
+      return new TimeSeries()
+    case '/maps':
+      return new Maps()
+    default:
+      return new TimeSeries()
   }
-  app.drawChart()
 }
